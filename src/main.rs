4be8e310c874aa582526c6341d55eaa93bdc72f8
SpaceMeerkat <james.d.astro@gmail.com>
2025-02-27 12:53:01 +0200

    basic header read and print functionality works

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use std::io::{BufReader, Read, Result};
 
 // Reads the first 2880 bytes (header).
 
-fn main() -> Result<()>{
-    // Declare the filepath
-    let file_path: &str = "datasets/example.fits";
-    println!("Filename set to {}.", &file_path);
-    // Define the file to open
+#[derive(Debug)]
+struct FitsHeader {
+    key: String,
+    value: String,
+}
+
+fn open_header(file_path: &str) -> Result<Vec<FitsHeader>> {
     let file = match File::open(file_path) {
         Ok(file) => file,
         Err(err) => {
@@ -31,6 +33,35 @@ fn main() -> Result<()>{
         }
     };
 
-    Ok(())
+    let mut headers = Vec::new();
+    // Define an empty vector to store the headers on the heap
 
+    for chunk in buffer.chunks(80) { // Each header entry has 80 bytes
+        let line = String::from_utf8_lossy(&chunk); // Changes a byte chunk into a string
+        if line.starts_with("END") { // If the last header key is END, then break the loop
+            break;
+        }
+        if let Some((key, rest)) = line.split_once("=") {
+            let key = key.trim().to_string();
+            let value = rest.split('/').next().unwrap_or("").trim().to_string();
+            headers.push(
+                FitsHeader {key: key, value: value}
+            );
+        }
+    }
+
+    Ok(headers)
+}
+
+fn main() -> Result<()> {
+    // Declare the filepath
+    let file_path: &str = "datasets/example.fits";
+    println!("Filename set to {}.", &file_path);
+    // Define the file to open
+    let headers = open_header(file_path);
+    for header in headers?.iter() {
+        println!("{:?}", header);
+    }
+    
+    Ok(())
 }
