4cd9d1e6d083aba7dc7fcdcfd84d2ddc9f3e98f6
SpaceMeerkat <james.d.astro@gmail.com>
2025-02-27 11:09:25 +0200

    first cargo commit

---
+++ target/debug/deps/faruspex-1bfdd1d42a622732.d
@@ -0,0 +1,5 @@
+/home/jdawson/repos/faruspex/target/debug/deps/faruspex-1bfdd1d42a622732: src/main.rs
+
+/home/jdawson/repos/faruspex/target/debug/deps/faruspex-1bfdd1d42a622732.d: src/main.rs
+
+src/main.rs:
